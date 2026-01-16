d9b823ed9251f9310b8f1dc8a3b07712da38a405
avadhutgiri12 <avadhutgiri123@gmail.com>
2026-01-16 21:10:17 +0530

    Refactor: move API keys to environment variables

--- chatbot_ollama.py
+++ chatbot_ollama.py
@@ -1,8 +1,14 @@
 import streamlit as st
-import ollama
+from groq import Groq
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-st.set_page_config(page_title="Ollama Chatbot", layout="centered")
-st.title("Ollama Chatbot")
+st.set_page_config(page_title="Flux Chatbot", layout="centered")
+st.title("Flux Chatbot")
+
+GROQ_API_KEY = os.getenv("API_KEY") # Replace with your actual key
+client = Groq(api_key=GROQ_API_KEY)
 
 # required to maintain the chat history
 if "messages" not in st.session_state:
@@ -14,31 +20,34 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
         
 #Default prompt or message
+def userInput():
+    prompt = st.chat_input("I am your AI Assistant. You can as me queries you like!!")
+    if prompt:
+        return prompt
+    else:
+        return "Hello!"
+    
 
-prompt = st.chat_input("I am your AI Assistant. You can as me queries you like!!")
-
+prompt = userInput()
 if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
         
 st.session_state.messages.append({"role":"user","content":prompt})
 
-#Fetch the response from ollama model
 with st.chat_message("assistant"):
     response_placeholder =st.empty()
     full_response =""
     
     try:
-        stream = ollama.chat(
-            model='gemma:2b',
-            messages= st.session_state.messages,
-            stream=True
+        chat_complition = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
         )
-        
-        for chunk in stream:
-            content = chunk['message']['content']
-            full_response +=content
-            response_placeholder.markdown(full_response+ " ")
+        full_response = chat_complition.choices[0].message.content
+        response_placeholder.markdown(full_response+ " ")
             
         response_placeholder.markdown(full_response)    
         st.session_state.messages.append({"role":"assistant","content" :full_response})
